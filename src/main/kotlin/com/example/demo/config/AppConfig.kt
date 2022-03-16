7c1044d983d51530c9ad50aae03f412a02b33b01
Christian Dräger <christian.draeger1@gmail.com>
2022-03-16 17:03:54 +0100

    wire repository bean and inject it to service

--- src/main/kotlin/com/example/demo/config/AppConfig.kt
+++ src/main/kotlin/com/example/demo/config/AppConfig.kt
@@ -1,5 +1,6 @@
 package com.example.demo.config
 
+import com.example.demo.repository.CourseRepository
 import com.example.demo.service.CourseService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -7,7 +8,10 @@ import org.springframework.context.annotation.Configuration
 @Configuration // <--- say spring that this class declares Beans
 class AppConfig {
 
+    @Bean("courseRepository")
+    fun getCourseRepository() = CourseRepository()
+
     @Bean("courseService") // optional name, default would be functions name
-    fun getCourseService() = CourseService() // if constructor signature would change we would only need to change it here
+    fun getCourseService() = CourseService(getCourseRepository()) // if constructor signature would change we would only need to change it here
 
 }
--- src/main/kotlin/com/example/demo/service/CourseService.kt
+++ src/main/kotlin/com/example/demo/service/CourseService.kt
@@ -3,13 +3,12 @@ package com.example.demo.service
 import com.example.demo.model.Course
 import com.example.demo.repository.CourseRepository
 
-class CourseService: CrudService<Course> {
-
-    // again instantiating an object with constructor on our own :(
-    private val repository: CourseRepository = CourseRepository()
+class CourseService(
+    private val courseRepository: CourseRepository
+): CrudService<Course> {
 
     override fun list(): List<Course> {
-        return repository.findAll()
+        return courseRepository.findAll()
     }
 
     override fun create(t: Course): Course {
